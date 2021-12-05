9eb450fcf6ad707a401d18ae1535ab417826d783
David Domingo Hansson <dadh@itu.dk>
2021-12-05 20:04:09 +0100

    Removed imports

--- src/main/java/grproproject/Main.java
+++ src/main/java/grproproject/Main.java
@@ -1,16 +1,10 @@
 package grproproject;
 
-import grproproject.controllers.HomeController;
-import grproproject.models.HomeModel;
-import grproproject.models.ProfilesModel;
 import grproproject.services.router.Router;
 import grproproject.services.router.Routes;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class Main extends Application {
 
