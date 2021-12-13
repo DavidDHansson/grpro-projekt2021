0720735190d2508235d60f1f34e2ed2c672eba39
David Domingo Hansson <dadh@itu.dk>
2021-12-13 12:58:41 +0100

    One test actually working

--- src/test/java/grproproject/managers/mediaManager/MediaManagerTest.java
+++ src/test/java/grproproject/managers/mediaManager/MediaManagerTest.java
@@ -3,6 +3,7 @@ package grproproject.managers.mediaManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MediaManagerTest {
 
@@ -20,9 +21,11 @@ class MediaManagerTest {
 
     @Test
     void getShuffledMedia() {
+        assertNotNull(MediaManager.getInstance().getShuffledMedia());
     }
 
     @Test
     void getGenresSorted() {
+        assertNotNull(MediaManager.getInstance().getGenresSorted());
     }
 }
